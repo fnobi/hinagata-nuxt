34e7c75c38d64cb9a230137b2c282a31c2aa6a6a
nobii <fujisawa-shin@kayac.com>
2017-11-25 23:18:30 +0900

    meta設定調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,30 @@
+const URL = 'http://example.com/';
+const TITLE = 'hinagata-nuxt';
+const DESCRIPTION = 'Nuxt.js project';
+const SHARE_IMAGE_URL = `${URL}ogp.png`;
+
 module.exports = {
   /*
   ** Headers of the page
   */
   head: {
-    title: 'hinagata-nuxt',
+    title: TITLE,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: DESCRIPTION },
+      { property: 'og:url', content: URL },
+      { property: 'og:image', content: SHARE_IMAGE_URL },
+      { property: 'og:title', content: TITLE },
+      { property: 'og:description', content: DESCRIPTION },
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:image', content: SHARE_IMAGE_URL },
+      { property: 'twitter:title', content: TITLE },
+      { property: 'twitter:description', content: DESCRIPTION }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'canonical', href: URL }
     ]
   },
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo/>
       <h1 class="title">
-        hinagata-nuxt
+        hinagata-nuxt test
       </h1>
       <h2 class="subtitle">
         Nuxt.js project
