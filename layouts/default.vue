a4008cb5b2d9154849f753764f201c21a9c202ec
nobii <fujisawa-shin@kayac.com>
2018-11-14 00:12:51 +0900

    ナビつくる

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,35 @@
 <template lang="pug">
-nuxt
+.wrapper
+  nuxt
+  nav.nav
+    .nav__item: nuxt-link(to="/") top
+    .nav__item: nuxt-link(to="/about") about
 </template>
+
+<style lang="scss">
+.nav {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 1em 0em;
+  text-align: center;
+}
+
+.nav__item {
+  &:after {
+    content: '|';
+    padding: 0em 1em;
+  }
+  &:last-child {
+    &:after {
+      display: none;
+    }
+  }
+  a {
+    color: inherit;
+  }
+}
+</style>
